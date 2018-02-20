dec3d4d40f727fefa0596fd842bc7aa07682f267
devry12 <devrykawiryan@gmail.com>
2018-02-20 12:26:50 +0700

    update database

--- db/seminar.sql
+++ db/seminar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 19, 2018 at 08:16 AM
+-- Generation Time: Feb 20, 2018 at 06:26 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -33,15 +33,19 @@ CREATE TABLE `admin` (
   `nama_admin` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `nim` varchar(10) NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` enum('Online','Offline') NOT NULL DEFAULT 'Offline'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id_admin`, `nama_admin`, `email`, `password`, `create_at`) VALUES
-(1, 'devry kawiryan', 'devrykawiryan@gmail.com', '$2y$10$sUr35wCBczFkX70DpYkY2enL2Lz6rw9ZLEozsH5/rX9rgNie9.OHC', '2018-02-19 06:59:30');
+INSERT INTO `admin` (`id_admin`, `nama_admin`, `email`, `password`, `nim`, `create_at`, `status`) VALUES
+(1, 'devry kawiryan', 'devrykawiryan@gmail.com', '$2y$10$sUr35wCBczFkX70DpYkY2enL2Lz6rw9ZLEozsH5/rX9rgNie9.OHC', '16.62.0090', '2018-02-19 06:59:30', 'Online'),
+(2, 'yogi yulianto', 'Yogi.yulianto@students.amikom.ac.id', '$2y$10$GxlMVzgNlzglfL7atzOTauBF8e1GRpcIla69mOHMT/AR0i7Ks5dJ2', '16.62.0091', '2018-02-20 01:56:13', 'Offline'),
+(3, 'lalalal', 'devry.k@students.amikom.ac.id', '$2y$10$IA9yAqMhRBqRUEgO99Ozk.cj7hleTeVHkC1bC97KQf9xgbQlyDvpi', '16.62.0092', '2018-02-20 03:02:16', 'Online');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +65,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
